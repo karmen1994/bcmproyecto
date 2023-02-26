7c7de315e72a9d12a15f1a5a965ab9b4e16c0c14
karmen1994 <93882528+karmen1994@users.noreply.github.com>
2023-02-26 14:06:16 +0000

    login maquetado
    hemos dado estilo al login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,69 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        #cajaForm{
+            border-radius:10px;
+            box-shadow: 0px 0px 8px silver;
+            padding: 10px;
+            box-sizing: border-box;
+        }
+    </style>
+    </head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  <div class="container">
+      <div class="row mt-5">
+        <div class="col text-center">
+            <h1 class="display-1">Control de Incidencias</h1>
+            <h2 class="display-6">IES José María Pérez Pulido</h2>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col mb-4 text-center">
+            @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col" id="cajaForm">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group m-4">
+                        <label for="email" class="form-label fs-4">Usuario</label>
+                        <input  id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                    </div>
+                    
+                    <div class="form-group m-4">
+                        <label class="form-label fs-4" for="password">Contraseña</label>
+                        <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
+                    </div>
+                    <button class="btn btn-primary mb-3">Acceder</button>
+
+                </form>
+            </div>
+            <div class="col-3"></div>
+        </div>
+            
+        </div>
+      </div>
+  </div>
+</body>
+</html>
+    {{-- <x-jet-authentication-card>
         <x-slot name="logo">
             {{-- <x-jet-authentication-card-logo /> --}}
-        </x-slot>
+        {{-- </x-slot> 
 
         <x-jet-validation-errors class="mb-4" />
 
@@ -23,7 +84,7 @@
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+            </div> --}}
 
             {{-- <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
@@ -32,17 +93,17 @@
                 </label>
             </div> --}}
 
-            <div class="flex items-center justify-end mt-4">
+            {{-- <div class="flex items-center justify-end mt-4"> --}}
                 {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif --}}
 
-                <x-jet-button class="ml-4">
+                {{-- <x-jet-button class=" ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
         </form>
-    </x-jet-authentication-card>
+    </x-jet-authentication-card> --}}
 </x-guest-layout>
